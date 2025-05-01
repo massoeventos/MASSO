354bbe0f41b511b0de131d022040ae3cf24bfce8
Javier Evans <evansjavier16@gmail.com>
2025-05-01 17:33:04 -0400

    feat: testing mail, missing config

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -15,6 +15,7 @@ class Kernel extends ConsoleKernel
     protected $commands = [
         Commands\SendNotifications::class,
         Commands\EmitTickets::class,
+        Commands\TestMail::class,
     ];
 
     /**
