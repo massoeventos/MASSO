17049689819631a5b9630f687517808047d2c912
Javier Evans <evansjavier16@gmail.com>
2025-05-01 17:30:58 -0400

    feat: testing mail

---
+++ app/Console/Commands/TestMail.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Mail;
+
+class TestMail extends Command
+{
+    protected $signature = 'test:mail';
+    protected $description = 'Enviar un correo de prueba';
+
+    public function handle()
+    {
+        Mail::raw('Este es un correo de prueba desde Laravel.', function ($message) {
+            $message->to(env('DEV_TESTING_MAIL'))
+                    ->subject('Correo de prueba');
+        });
+
+        $this->info('Correo de prueba enviado.');
+    }
+}
