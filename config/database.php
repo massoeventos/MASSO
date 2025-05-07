60adb28ed31c0ebbfdddeb7d0cc95b027a9c05f0
Javier Evans <evansjavier16@gmail.com>
2025-05-06 21:32:15 -0400

    feat: migrations and seeder for countries, regions and cities

--- config/database.php
+++ config/database.php
@@ -61,6 +61,7 @@ return [
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
             'strict'    => false,
+            'engine'    => 'InnoDB'
         ],
 
         'pgsql' => [
---
+++ database/migrations/2025_05_06_195118_create_regions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRegionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('regions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('country_id')->unsigned(); // Relación con la tabla de países
+            $table->string('name');
+            $table->boolean('is_other')->default(false); // Indica si es "Otro"
+            $table->timestamps();
+
+            $table->foreign('country_id')->references('id')->on('countries')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('regions');
+    }
+}
