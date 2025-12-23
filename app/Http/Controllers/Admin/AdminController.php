73f7931429ddac7ab6dd3ca61c729478b10c8eda
Javier Evans <evansjavier16@gmail.com>
2025-12-23 16:57:41 -0400

    fix: evitando error por authUser null

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -2,6 +2,8 @@
 namespace Masso\Http\Controllers\Admin;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -12,17 +14,18 @@ abstract class AdminController extends BaseController
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
     protected $user;
 
-    public function __construct( Request $request ){
+    public function __construct(Request $request)
+    {
+        $this->middleware(function ($req, $next) {
+            $route = $this->route = $req->route() ? $req->route()->getName() : '';
 
-        $route = $this->route = '';
-        if(!is_null($request->route()) )
-            $route = $this->route = $request->route()->getName();
+            $authUser = $this->user = Auth::user();
 
-        \View::share('authUser', $authUser = $this->user = \Auth::user());
-        \View::share('title', trans('titles.'.str_replace('.', '_', $route)));
+            \View::share('authUser', $authUser);
+            \View::share('title', trans('titles.' . str_replace('.', '_', $route)));
+            \View::share('croute', $route);
 
-		\View::share('croute', $route);
-
-
+            return $next($req);
+        });
     }
 }
