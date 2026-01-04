eb256aae596d1a2227821a8369d7430e3f446c58
Javier Evans <evansjavier16@gmail.com>
2026-01-04 16:54:00 -0400

    feat: mostrar errores en ventana de alerta

--- resources/views/admin/common/flash.blade.php
+++ resources/views/admin/common/flash.blade.php
@@ -5,3 +5,13 @@
 @if(Session::has('success_alert'))
 	<div class="alert alert-success" style="color: black;">{!! Session::get('success_alert') !!}</div>
 @endif
+
+@if ($errors->any())
+	<div class="alert alert-danger flash-alert">
+		<ul class="mb-0">
+			@foreach ($errors->all() as $error)
+				<li>{{ $error }}</li>
+			@endforeach
+		</ul>
+	</div>
+@endif
